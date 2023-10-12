3f2a7122300547be8688b1f7d5e21b84dd965e65
migue <miguelito.gomes.silva@gmai.com>
2023-10-12 10:09:56 +0100

    exceções

--- src/aed/analisecomplexidade/algoritmos/JogoTorresHanoi.java
+++ src/aed/analisecomplexidade/algoritmos/JogoTorresHanoi.java
@@ -1,33 +1,37 @@
 package aed.analisecomplexidade.algoritmos;
 public class JogoTorresHanoi {
-    private static int mov;
-    public static void executar(int n, char torre_inicio, char torre_auxiliar, char torre_destino) {
-        if (n < 1) {
-            throw new IllegalArgumentException("Argumento " + n + " Inválido");
+    public static void executar(int numeroDiscos){
+        if (numeroDiscos < 1) {
+            throw new IllegalArgumentException("Argumento " + numeroDiscos + " Inválido");
         }
-        mov=0;
-        executarRecursivo(n, torre_inicio, torre_auxiliar, torre_destino);
+        Torre torre_A =new Torre('A',numeroDiscos);
+        Torre torre_B =new Torre('B',numeroDiscos);
+        Torre torre_C =new Torre('C',numeroDiscos);
+        torre_A.preencherComDiscos();
+
+        executarRecursivo(numeroDiscos, torre_A, torre_B, torre_C);
     }
 
-    private static void executarRecursivo(int n, char torre_inicio, char torre_auxiliar, char torre_destino) {
+    private static void executarRecursivo(int n, Torre torre_inicio, Torre torre_auxiliar, Torre torre_destino){
         if (n < 2) {
-            moverDisco(n, torre_inicio, torre_destino);
+            //overDisco(n, torre_inicio, torre_destino);
+            torre_inicio.moverDisco(torre_destino);
             return;
         }
         executarRecursivo(n - 1, torre_inicio, torre_destino, torre_auxiliar);
-        moverDisco(n,torre_inicio,torre_destino);
+        //moverDisco(n,torre_inicio,torre_destino);
+        torre_inicio.moverDisco(torre_destino);
         executarRecursivo(n - 1, torre_auxiliar, torre_inicio, torre_destino);
     }
 
     private static void moverDisco(int n, char torre_inicio, char torre_destino) {
         System.out.println("Mover disco: " + n + " da torre " + torre_inicio + " para a torre " + torre_destino);
-        mov +=1;
-        System.out.println("Mov: "+ mov);
+
     }
 
-    public static void apresentar(int n, char torre_inicio, char torre_destino, char torre_auxiliar) {
+    public static void apresentar(int n) {
         try {
-            executar(n, torre_inicio, torre_destino, torre_auxiliar);
+            executar(n);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
--- src/aed/analisecomplexidade/algoritmos/Torre.java
+++ src/aed/analisecomplexidade/algoritmos/Torre.java
@@ -1,38 +1,50 @@
 package aed.analisecomplexidade.algoritmos;
 
+import java.util.NoSuchElementException;
+
 public class Torre {
     private char nome;
     private int numeroDiscos;
     private int[] baseEDiscos;
-    public Torre(char nome,int numeroMaxDiscos){
-        this.nome=nome;
-        this.numeroDiscos=0;
-        this.baseEDiscos= new int[numeroMaxDiscos+1];
-        this.baseEDiscos[0]=3*numeroMaxDiscos;
-    }
-    public void preencherComDiscos(){
-        for (int i = 1; i < baseEDiscos.length; i++) {
-            baseEDiscos[i]= baseEDiscos.length-i;
-        }
-        numeroDiscos=baseEDiscos.length-1;
+
+    public Torre(char nome, int numeroMaxDiscos) {
+        this.nome = nome;
+        this.numeroDiscos = 0;
+        this.baseEDiscos = new int[numeroMaxDiscos + 1];
+        this.baseEDiscos[0] = 3 * numeroMaxDiscos;
     }
 
-    public void moverDisco(Torre torreDestina) throws Exception {
-        if(this.numeroDiscos==0) {
-            throw new Exception("A torre   está vazia");
+    public void preencherComDiscos() {
+        int length = baseEDiscos.length;
+        for (int i = 1; i < length; i++) {
+            baseEDiscos[i] = length - i;
         }
-        if(torreDestina.numeroDiscos==torreDestina.baseEDiscos.length){
-            throw new Exception("A torre destino está cheia");
+        numeroDiscos = length - 1;
+    }
+
+    public void moverDisco(Torre torreDestina)  {
+        if (this.numeroDiscos == 0) {
+            throw new ArrayIndexOutOfBoundsException("A torre   está vazia");
         }
+        if (torreDestina.numeroDiscos == torreDestina.baseEDiscos.length - 1) {
+            throw new NoSuchElementException("A torre destino está cheia");
+        }
+        if(this.baseEDiscos[this.numeroDiscos]<= torreDestina.baseEDiscos[torreDestina.numeroDiscos]){
+            throw new IllegalArgumentException("Disco a mover não menor do que o disco do topo da torre de destino");
+        }
+
 
         //a 1 e 4 podem ser juntas desta forma
-        torreDestina.baseEDiscos[++torreDestina.numeroDiscos]=this.baseEDiscos[this.numeroDiscos];
+        torreDestina.baseEDiscos[++torreDestina.numeroDiscos] = this.baseEDiscos[this.numeroDiscos];
+        //a 2 e 3 podem ser juntas assim
+        this.baseEDiscos[numeroDiscos--] = 0;
+
         // 1
         //torreDestina.baseEDiscos[torreDestina.numeroDiscos+1]=this.baseEDiscos[this.numeroDiscos];
         // 2
-        this.baseEDiscos[numeroDiscos]=0;
+        //this.baseEDiscos[numeroDiscos]=0;
         // 3
-        this.numeroDiscos--;
+        //this.numeroDiscos--;
         // 4 pode ser feita antes da numero 2 tbm
         // torreDestina.numeroDiscos++;
     }
