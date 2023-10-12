8c7db62c7a149897c6b8d32ad7496ae6e87a45b7
migue <miguelito.gomes.silva@gmai.com>
2023-10-12 09:35:12 +0100

    algoritmos moment

---
+++ src/aed/analisecomplexidade/algoritmos/Torre.java
@@ -0,0 +1,37 @@
+package aed.analisecomplexidade.algoritmos;
+
+public class Torre {
+    private char nome;
+    private int numeroDiscos;
+    private int[] baseEDiscos;
+    public Torre(char nome,int numeroMaxDiscos){
+        this.nome=nome;
+        this.numeroDiscos=0;
+        this.baseEDiscos= new int[numeroMaxDiscos+1];
+        this.baseEDiscos[0]=3*numeroMaxDiscos;
+    }
+    public void preencherComDiscos(){
+        for (int i = 1; i < baseEDiscos.length; i++) {
+            baseEDiscos[i]= baseEDiscos.length-i;
+        }
+        numeroDiscos=baseEDiscos.length-1;
+    }
+
+    public void moverDisco(Torre torreDestina) throws Exception {
+        if(this.numeroDiscos==0) {
+            throw new Exception("A torre   está vazia");
+        }
+        if(torreDestina.numeroDiscos==torreDestina.baseEDiscos.length){
+            throw new Exception("A torre destino está cheia");
+        }
+        // 1
+        torreDestina.baseEDiscos[numeroDiscos+1]=this.baseEDiscos[this.numeroDiscos];
+        // 2
+        this.baseEDiscos[numeroDiscos]=0;
+        // 3
+        this.numeroDiscos--;
+        // 4 pode ser feita antes da numero 2 tbm
+        torreDestina.numeroDiscos++;
+    }
+
+}
---
+++ src/aed/analisecomplexidade/utilizacao/MainPraticaHanoi.java
@@ -0,0 +1,13 @@
+package aed.analisecomplexidade.utilizacao;
+
+
+import aed.recursividade.algoritmos.JogoTorresHanoi;
+
+public class MainPraticaHanoi {
+    public static void main(String[] args) {
+        JogoTorresHanoi.apresentar(5,'A','B','C');
+        JogoTorresHanoi.apresentar(4,'A','B','C');
+        JogoTorresHanoi.apresentar(0,'A','B','C');
+        JogoTorresHanoi.apresentar(3,'A','B','C');
+    }
+}
